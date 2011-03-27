02589e2f77025903e10038d3f9e9d30e2893c0f6
Chia-Ling Lee <spiderman0103@gmail.com>
2011-03-27 15:14:39 +0800

    First commit initially.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Msgboard::Application.config.secret_token = '935774167c3b5b51140561971bd1f48a7447aac673075265d00f7d039463944999c440ec26d6f2660b2a3927841e33cc5d082df17930b5c1916a2a0ac77b8535'
